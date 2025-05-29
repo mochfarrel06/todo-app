f25beaa42907bc319ca8d53597d557b922d13a7c
MochFarrel06 <mochfarrel2606@gmail.com>
2025-05-30 00:17:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4">
+    <nav class="flex gap-4">
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+</script>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        data() {
+            return {
+                text: '',
+                category: 'Category'
+            }
+        },
+        methods: {
+            submit() {
+                if (!this.text.trim()) return;
+
+                this.$emit('add-todo', {
+                    id: Date.now(),
+                    text: this.text,
+                    category: this.category,
+                    completed: false
+                })
+
+                this.text = ''
+            }
+        }
+    }
+</script>
+
+<template>
+    <form @submit.prevent="submit">
+        <input type="text" v-model="text" placeholder="Tugas..." class="border p-2">
+        <select v-model="category" class="border p-2">
+            <option>Work</option>
+            <option>Personal</option>
+        </select>
+
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2">Add</button>
+    </form>
+</template>
