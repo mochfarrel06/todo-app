5243b6730ba540000c4784ab7c634f3579fa0a87
MochFarrel06 <mochfarrel2606@gmail.com>
2025-05-30 09:34:23 +0700

    main/menambahkan component todo item

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        props: {
+            todo: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<template>
+    <li :class="{ 'line-trough text-gray-400' : todo.completed }" class="flex justify-between items-center border p-2">
+        <div>
+            <input type="checkbox" :checked="todo.completed" @change="$emit('toggle')" class="mr-2" />
+            {{ todo.text }} {{ todo.category }}
+        </div>
+
+        <button @click="$emit('delete')" class="text-red-500">X</button>
+    </li>
+</template>
