9a83d219d7cd9a08049c695685ca0aeea0b25d98
MochFarrel06 <mochfarrel2606@gmail.com>
2025-05-30 09:51:03 +0700

    main/menambahkan component todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<script>
+    import TodoItem from './TodoItem.vue';
+
+    export default {
+        components: {
+            TodoItem
+        },
+        props: {
+            todos: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<template>
+    <ul class="space-y-2">
+        <TodoItem 
+            v-for="todo in todos"
+            :key="todo.id"
+            :todo="todo"
+            @toggle="() => $emit('toggle-todo', todo.id)"
+            @delete="() => $emit('delete-todo', todo.id)"
+        />
+    </ul>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,7 @@
+<script>
+    import TodoForm from '@/components/TodoForm.vue';
+</script>
+
 <template>
     <h1>Home</h1>
 </template>
