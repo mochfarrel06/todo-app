e66a7a7aa587ce01a177179027fb57e8dc4ba887
MochFarrel06 <mochfarrel2606@gmail.com>
2025-05-30 09:46:11 +0700

    main/menambahkan component filter select

---
+++ src/components/FilterSelect.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        props: {
+            modelValue: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            updateValue(value) {
+                this.$emit('update:modelValue', value)
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="my-4">
+        <label class="mr-2">Filter:</label>
+        <select :value="modelValue" @input="updateValue($event.target.value)" class="border p-2">
+            <option>All</option>
+            <option>Work</option>
+            <option>Personal</option>
+        </select>
+    </div>
+</template>
